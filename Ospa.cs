90e50df300796ae7f48ae95524bdc9b4c2f94504
akellai <ab@>
2023-11-07 14:58:06 +0100

    Initial commit - POC

---
+++ Ospa.cs
@@ -0,0 +1,459 @@
+﻿using System;
+using System.Text;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronWebSocketClient;
+using Crestron.SimplSharp.Cryptography;
+using Newtonsoft.Json;
+
+namespace OspaSS
+{
+    public class Ospa
+    {
+        public delegate void StatusToSPlusHandler(int arg);
+        public StatusToSPlusHandler OnError { set; get; }
+        public StatusToSPlusHandler OnConnectChange { set; get; }
+        public StatusToSPlusHandler OnGrantedChange { set; get; }
+
+        public delegate void ValueToSPlusHandler(int arg, int val);
+        public ValueToSPlusHandler OnValueReported { set; get; }
+
+        public delegate void DebugLogHandler(SimplSharpString data);
+        public DebugLogHandler OnLog { set; get; }
+
+        private string m_accessKey;
+        private string m_accessKeyBack;
+        private bool m_allowed;
+        private CTimer keepAliveTimer;
+        internal WebSocketClient wsc;
+
+        public Ospa() { }
+
+        private void init()
+        {
+            m_allowed = false;
+            if (OnGrantedChange != null)
+                OnGrantedChange( m_allowed?1:0 );
+            m_accessKey = null;
+            m_accessKeyBack = null;
+        }
+
+        private bool m_debug = false;
+        public int Debug
+        {
+            set { m_debug = value != 0; }
+            get { return m_debug ? 1 : 0; }
+        }
+
+        private const int maxChunkSize = 250;
+        public void Log(string message, params object[] arg)
+        {
+            if (m_debug)
+            {
+                if (OnLog != null)
+                {
+                    string sdata = string.Format(message, arg) + "\n";
+
+                    for (int i = 0; i < sdata.Length; i += maxChunkSize)
+                    {
+                        string schunk = sdata.Substring(i, Math.Min(maxChunkSize, sdata.Length - i));
+                        OnLog(new SimplSharpString(schunk));
+                    }
+                }
+            }
+        }
+
+        private WebSocketClient.WEBSOCKET_RESULT_CODES ws_send(string resp)
+        {
+            Log("->| {0}", resp);
+            byte[] bresp = Encoding.ASCII.GetBytes(resp);
+            return wsc.Send(bresp, (uint)bresp.Length, WebSocketClient.WEBSOCKET_PACKET_TYPES.LWS_WS_OPCODE_07__TEXT_FRAME);
+        }
+
+        private WebSocketClient.WEBSOCKET_RESULT_CODES ws_json_send(object ws_obj)
+        {
+            string s = JsonConvert.SerializeObject(ws_obj, Formatting.None, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            return ws_send(s);
+        }
+
+        private const string m_heartbeat = "{\"m_TimeoutHandler\":\"test\"}";
+        private void OnTimer(Object o)
+        {
+            if (wsc.Connected)
+            {
+                try
+                {
+                    ws_send(m_heartbeat);
+                    keepAliveTimer.Reset(180000);
+                }
+                catch (Exception e)
+                {
+                    Log(e.Message);
+                }
+            }
+        }
+
+        private int onDisconnect(WebSocketClient.WEBSOCKET_RESULT_CODES error, object obj)
+        {
+            Log("Disconnected {0}", DateTime.Now.ToString());
+            if (OnConnectChange != null)
+                OnConnectChange(0);
+            if (OnError != null)
+                OnError((int) error);
+            init();
+            return (int) error;
+        }
+
+        private int onConnect(WebSocketClient.WEBSOCKET_RESULT_CODES error)
+        {
+            Log("Connected {0}", DateTime.Now.ToString());
+            wsc.ReceiveAsync();
+            if (OnConnectChange != null)
+                OnConnectChange(1);
+            if (OnError != null)
+                OnError((int)error);
+            return (int)error;
+        }
+
+        private int int10(float x)
+        {
+            return (int) ((x + 0.01) * 10.0);
+        }
+
+        private int int100(double x)
+        {
+            return (int)((x + 0.001) * 100.0);
+        }
+
+        internal void reportValue(int num, bool? val)
+        {
+            if (OnValueReported != null)
+                if (val != null)
+                    OnValueReported(num,(val ?? false)?1:0);
+        }
+
+        internal void reportValue(int num, int? val)
+        {
+            if (OnValueReported != null)
+                if (val != null)
+                    OnValueReported(num, val ?? 0);
+        }
+
+        internal void reportValue(int num, float? val)
+        {
+            if (OnValueReported != null)
+                if (val != null)
+                    OnValueReported(num, int10( val ?? 0) );
+        }
+
+        // 'smart' use of double vs float:)))
+        internal void reportValue(int num, double? val)
+        {
+            if (OnValueReported != null)
+                if (val != null)
+                    OnValueReported(num, int100(val ?? 0));
+        }
+
+        public void Send(int arg, int val)
+        {
+            OspaJson.wsMessage msg = new OspaJson.wsMessage();
+            switch (arg)
+            {
+                case 1:
+                    msg.m_TempIst_1_2 = val / 10f;
+                    break;
+                case 2:
+                    msg.m_RedoxIst = val;
+                    break;
+                case 3:
+                    msg.m_pHIst = val / 100d;
+                    break;
+                case 4:
+                    msg.m_ChlorIst = val / 10f;
+                    break;
+                case 5:
+                    msg.m_TempSoll_1 = val / 10f;
+                    break;
+                case 6:
+                    msg.m_TempSoll_2 = val / 10f;
+                    break;
+                case 7:
+                    msg.m_RaumTempIst = val / 10f; ;
+                    break;
+                case 8:
+                    msg.m_RaumTempSoll = val / 10f;
+                    break;
+                case 9:
+                    msg.m_RaumFeuchteIst = val / 10f;
+                    break;
+                case 10:
+                    msg.m_RaumFeuchteSoll = val / 10f;
+                    break;
+                case 11:
+                    msg.m_WasserwerteGueltig = val != 0;
+                    break;
+                case 12:
+                    msg.m_AttrBetrieb1 = val != 0;
+                    break;
+                case 13:
+                    msg.m_AttrBetrieb2 = val != 0;
+                    break;
+                case 14:
+                    msg.m_AttrBetrieb3 = val != 0;
+                    break;
+                case 15:
+                    msg.m_AttrBetrieb4 = val != 0;
+                    break;
+                case 16:
+                    msg.m_AttrBetrieb5 = val != 0;
+                    break;
+                case 17:
+                    msg.m_AttrBetrieb6 = val != 0;
+                    break;
+                case 18:
+                    msg.m_AttrBetrieb7 = val != 0;
+                    break;
+                case 19:
+                    msg.m_AttrBetrieb8 = val != 0;
+                    break;
+                case 20:
+                    msg.m_AttrBetrieb9 = val != 0;
+                    break;
+                case 21:
+                    msg.m_AttrBetrieb10 = val != 0;
+                    break;
+                case 22:
+                    msg.m_Uws1SbEin = val != 0;
+                    break;
+                case 23:
+                    msg.m_Uws1WpEin = val != 0;
+                    break;
+                case 24:
+                    msg.m_WpEin = val != 0;
+                    break;
+                case 25:
+                    msg.m_SbEin = val != 0;
+                    break;
+                case 26:
+                    msg.m_RinneEin = val != 0;
+                    break;
+                case 27:
+                    msg.m_BodenreinigerEin = val != 0;
+                    break;
+                case 28:
+                    msg.m_FilteranlageBetrieb = val != 0;
+                    break;
+                case 29:
+                    msg.m_StoerungWasserwerte = val != 0;
+                    break;
+                case 30:
+                    msg.m_Sammelstoerung = val != 0;
+                    break;
+                case 31:
+                    msg.m_LichtSzene = val;
+                    break;
+                case 32:
+                    msg.m_FilterBetriebsart = val;
+                    break;
+                case 33:
+                    msg.m_KlimaBetrieb = val != 0;
+                    break;
+                case 34:
+                    msg.m_KlimaBadebetrieb = val != 0;
+                    break;
+                case 35:
+                    msg.m_KlimaFrischluftbetrieb = val != 0;
+                    break;
+                case 36:
+                    msg.m_AlarmeAnstehend = val != 0;
+                    break;
+                case 37:
+                    msg.m_FilterpumpeVoruebergehendSchalten = val;
+                    break;
+                case 38:
+                    msg.m_TempRegler1_Aktiv = val != 0;
+                    break;
+                case 39:
+                    msg.m_RedoxSoll = val;
+                    break;
+                case 40:
+                    msg.m_pHSoll = val / 100d;
+                    break;
+                case 41:
+                    msg.m_ChlorSoll = val / 10f;
+                    break;
+                case 42:
+                    msg.m_KlimaFrischluftschaltung = val != 0;
+                    break;
+                case 43:
+                    msg.m_AbschaltenWegenDurchflussmangel = val != 0;
+                    break;
+                case 44:
+                    msg.m_AlarmMaxFrischwasserNachspeisung = val != 0;
+                    break;
+                case 45:
+                    msg.m_AlarmLeckageErkannt = val != 0;
+                    break;
+                case 46:
+                    msg.m_ErrTestAbsperrhahn1 = val != 0;
+                    break;
+            }
+            if (m_allowed)
+                ws_json_send(msg);
+        }
+
+        private void processMessage(string rcvString)
+        {
+            try
+            {
+                OspaJson.wsMessage msg = JsonConvert.DeserializeObject<OspaJson.wsMessage>(rcvString);
+                reportValue(1, msg.m_TempIst_1_2);
+                reportValue(2, msg.m_RedoxIst);
+                reportValue(3, msg.m_pHIst);
+                reportValue(4, msg.m_ChlorIst);
+                reportValue(5, msg.m_TempSoll_1);
+                reportValue(6, msg.m_TempSoll_2);
+                reportValue(7, msg.m_RaumTempIst);
+                reportValue(8, msg.m_RaumTempSoll);
+                reportValue(9, msg.m_RaumFeuchteIst);
+                reportValue(10, msg.m_RaumFeuchteSoll);
+                reportValue(11, msg.m_WasserwerteGueltig);
+                reportValue(12, msg.m_AttrBetrieb1);
+                reportValue(13, msg.m_AttrBetrieb2);
+                reportValue(14, msg.m_AttrBetrieb3);
+                reportValue(15, msg.m_AttrBetrieb4);
+                reportValue(16, msg.m_AttrBetrieb5);
+                reportValue(17, msg.m_AttrBetrieb6);
+                reportValue(18, msg.m_AttrBetrieb7);
+                reportValue(19, msg.m_AttrBetrieb8);
+                reportValue(20, msg.m_AttrBetrieb9);
+                reportValue(21, msg.m_AttrBetrieb10);
+                reportValue(22, msg.m_Uws1SbEin);
+                reportValue(23, msg.m_Uws1WpEin);
+                reportValue(24, msg.m_WpEin);
+                reportValue(25, msg.m_SbEin);
+                reportValue(26, msg.m_RinneEin);
+                reportValue(27, msg.m_BodenreinigerEin);
+                reportValue(28, msg.m_FilteranlageBetrieb);
+                reportValue(29, msg.m_StoerungWasserwerte);
+                reportValue(30, msg.m_Sammelstoerung);
+                reportValue(31, msg.m_LichtSzene);
+                reportValue(32, msg.m_FilterBetriebsart);
+                reportValue(33, msg.m_KlimaBetrieb);
+                reportValue(34, msg.m_KlimaBadebetrieb);
+                reportValue(35, msg.m_KlimaFrischluftbetrieb);
+                reportValue(36, msg.m_AlarmeAnstehend);
+                reportValue(37, msg.m_FilterpumpeVoruebergehendSchalten);
+                reportValue(38, msg.m_TempRegler1_Aktiv);
+                reportValue(39, msg.m_RedoxSoll);
+                reportValue(40, msg.m_pHSoll);
+                reportValue(41, msg.m_ChlorSoll);
+                reportValue(42, msg.m_KlimaFrischluftschaltung);
+                reportValue(43, msg.m_AbschaltenWegenDurchflussmangel);
+                reportValue(44, msg.m_AlarmMaxFrischwasserNachspeisung);
+                reportValue(45, msg.m_AlarmLeckageErkannt);
+                reportValue(46, msg.m_ErrTestAbsperrhahn1);        
+            }
+
+            catch (Exception e)
+            {
+                Log("processMessage: {0}",e.Message);
+            }
+        }
+
+        private void processAllowed(string rcvString)
+        {
+            try
+            {
+                OspaJson.AccessAllowed msg = JsonConvert.DeserializeObject<OspaJson.AccessAllowed>(rcvString);
+                m_allowed = msg.accessAllowed;
+                if (OnGrantedChange != null)
+                    OnGrantedChange(m_allowed ? 1 : 0);
+            }
+            catch (Exception e)
+            {
+                Log("processAllowed: {0}", e.Message);
+            }
+        }
+
+        private void logon(string rcvString)
+        {
+            try
+            {
+                OspaJson.AccessKey key = JsonConvert.DeserializeObject<OspaJson.AccessKey>(rcvString);
+                m_accessKey = key.accessKey;
+
+                SHA1Managed sh1 = new SHA1Managed();
+                byte[] accessKeyBack = sh1.ComputeHash(Encoding.ASCII.GetBytes(m_accessKey));
+                m_accessKeyBack = BitConverter.ToString(accessKeyBack).Replace("-", string.Empty).ToLower();
+
+                OspaJson.AccessKeyBack keyBack = new OspaJson.AccessKeyBack();
+                keyBack.accessKeyBack = m_accessKeyBack;
+                ws_json_send(keyBack);
+                keepAliveTimer.Reset(180000);
+            }
+            catch (Exception e)
+            {
+                m_accessKey = null;
+                Log("logon: {0}", e.Message);
+            }
+        }
+
+        private int onReceive(byte[] data, uint datalen, WebSocketClient.WEBSOCKET_PACKET_TYPES opcode, WebSocketClient.WEBSOCKET_RESULT_CODES error)
+        {
+            if (error != WebSocketClient.WEBSOCKET_RESULT_CODES.WEBSOCKET_CLIENT_SUCCESS)
+            {
+                wsc.DisconnectAsync(this);
+                return (int)error;
+            }
+
+            string rcvString = Encoding.ASCII.GetString(data, 0, data.Length);
+            Log("<-| {0}", rcvString);
+
+            if (string.IsNullOrEmpty(m_accessKey))
+            {
+                logon(rcvString);
+            }
+            else
+            {
+                if( m_allowed )
+                    processMessage(rcvString);
+                else
+                    processAllowed(rcvString);
+            }
+
+            wsc.ReceiveAsync();
+            return (int)error;
+        }
+
+        public void Enable(int ienable)
+        {
+            if (ienable == 0)
+            {
+                if (wsc.Connected)
+                {
+                    wsc.DisconnectAsync(this);
+                }
+            }
+            else
+            {
+                if (!wsc.Connected)
+                {
+                    wsc.ConnectAsync();
+                }
+            }
+        }
+
+        public void Initialize(string host)
+        {
+            init();
+            keepAliveTimer = new CTimer(OnTimer, Timeout.Infinite);
+            wsc = new WebSocketClient();
+            wsc.URL = "ws://" + host + ":56525/user";
+            wsc.Host = host + ":56525";
+            wsc.Origin = "http://".ToString() + host + ":56525";
+            wsc.DisconnectCallBack += onDisconnect;
+            wsc.ReceiveCallBack += onReceive;
+            wsc.ConnectionCallBack += onConnect;
+        }
+    }
+}
---
+++ OspaJson.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OspaSS
+{
+    public class OspaJson
+    {
+        public class AccessKey
+        {
+            public string accessKey { get; set; }
+        }
+
+        public class AccessAllowed
+        {
+            public bool accessAllowed { get; set; }
+        }
+
+        public class AccessKeyBack
+        {
+            public string accessKeyBack { get; set; }
+        }
+
+        public class wsMessage
+        {
+            public float? m_TempIst_1_2 { get; set; }          // 1
+            public int? m_RedoxIst { get; set; }               // 2
+            public double? m_pHIst { get; set; }               // 3
+            public float? m_ChlorIst { get; set; }             // 4
+            public float? m_TempSoll_1 { get; set; }           // 5
+            public float? m_TempSoll_2 { get; set; }           // 6
+            public float? m_RaumTempIst { get; set; }          // 7
+            public float? m_RaumTempSoll { get; set; }         // 8
+            public float? m_RaumFeuchteIst { get; set; }       // 9
+            public float? m_RaumFeuchteSoll { get; set; }      // 10
+            public bool? m_WasserwerteGueltig { get; set; }    // 11
+            public bool? m_AttrBetrieb1 { get; set; }          // 12
+            public bool? m_AttrBetrieb2 { get; set; }          // 13
+            public bool? m_AttrBetrieb3 { get; set; }          // 14
+            public bool? m_AttrBetrieb4 { get; set; }          // 15
+            public bool? m_AttrBetrieb5 { get; set; }          // 16
+            public bool? m_AttrBetrieb6 { get; set; }          // 17
+            public bool? m_AttrBetrieb7 { get; set; }          // 18
+            public bool? m_AttrBetrieb8 { get; set; }          // 19
+            public bool? m_AttrBetrieb9 { get; set; }          // 20
+            public bool? m_AttrBetrieb10 { get; set; }         // 21
+            public int[] m_AttrFuStufen { get; set; }
+            public bool? m_Uws1SbEin { get; set; }             // 22
+            public bool? m_Uws1WpEin { get; set; }             // 23
+            public bool? m_WpEin { get; set; }                 // 24
+            public bool? m_SbEin { get; set; }                 // 25
+            public bool? m_RinneEin { get; set; }              // 26
+            public bool? m_BodenreinigerEin { get; set; }      // 27
+            public bool? m_FilteranlageBetrieb { get; set; }   // 28
+            public bool? m_StoerungWasserwerte { get; set; }   // 29
+            public bool? m_Sammelstoerung { get; set; }        // 30
+            public int? m_LichtSzene { get; set; }             // 31
+            public int? m_FilterBetriebsart { get; set; }      // 32
+            public bool? m_KlimaBetrieb { get; set; }          // 33
+            public bool? m_KlimaBadebetrieb { get; set; }      // 34
+            public bool? m_KlimaFrischluftbetrieb { get; set; }// 35
+            public bool? m_AlarmeAnstehend { get; set; }       // 36
+            public int[] m_FilterLaufzeiten { get; set; }
+            public int? m_FilterpumpeVoruebergehendSchalten { get; set; }  // 37
+            public int[] m_AppAttrLeistungsEinstellung { get; set; }
+            public int[] m_AppPsTrainingsprogramm { get; set; }
+            public int[] m_AppAbdeckung { get; set; }
+            public int[] m_AppLichtSzene { get; set; }
+            public bool? m_TempRegler1_Aktiv { get; set; }     // 38
+            public int? m_RedoxSoll { get; set; }              // 39
+            public double? m_pHSoll { get; set; }              // 40
+            public float? m_ChlorSoll { get; set; }            // 41
+            public bool? m_KlimaFrischluftschaltung { get; set; }          // 42
+            public bool? m_AbschaltenWegenDurchflussmangel { get; set; }   // 43
+            public bool? m_AlarmMaxFrischwasserNachspeisung { get; set; }  // 44
+            public bool? m_AlarmLeckageErkannt { get; set; }               // 45
+            public bool? m_ErrTestAbsperrhahn1 { get; set; }               // 46
+        }
+    }
+}
